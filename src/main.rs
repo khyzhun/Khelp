a87d2032012bd285b8c4e638d81dae79df8bcb80
khyzhun <sasha.khyzhun@gmail.com>
2025-04-15 22:46:39 +0300

    added test command you open youtube;

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,21 @@ use std::process::Command;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    if args.len() == 3 && args[1] == "-o" && args[2] == "youtube" {
+        let _ = Command::new("open")
+            .arg("https://www.youtube.com/")
+            .status()
+            .expect("failed to open browser");
+        return;
+    }
+
     if args.len() == 4 && args[1] == "-o" && args[2] == "store" {
         let package = &args[3];
         let url = format!("https://play.google.com/store/apps/details?id={}", package);
-        let _ = Command::new("open")  // for macOS
+        let _ = Command::new("open")
             .arg(url)
             .status()
             .expect("failed to open browser");
-    } else {
-        eprintln!("Usage: khelper -o store <package_name>");
+        return;
     }
 }
